2d8caf57b1b643655999ba8691ad0c6a32c30b4c
Dana Milena Erro <danamerro@gmail.com>
2026-06-27 02:24:03 -0300

    Se agrega triggers de validacion y normalizacion

--- Script/Script_triggers.sql
+++ Script/Script_triggers.sql
@@ -1,29 +1,64 @@
 USE LaboratorioEnsayosElectricos;
 GO
 
-CREATE TRIGGER TR_Crear_Registro_PostIngreso ON INGRESOS
-AFTER INSERT
+
+DROP TRIGGER IF EXISTS TR_Validar_Empleado_TipoEnsayo;
+GO
+
+CREATE TRIGGER TR_Validar_Empleado_TipoEnsayo
+ON REGISTROS
+AFTER INSERT, UPDATE
 AS
 BEGIN
-	INSERT INTO REGISTROS (
-		IdIngreso,
-		IdTipoEnsayo,
-		Resultado,
-		FechaEnsayo,
-		Humedad,
-		Temperatura,
-		Observaciones,
-		IdEmpleado
-	)
-	SELECT 
-        IdIngreso,
-        '',
-        'ESPERANDO ENSAYO',
-        GETDATE(),
-        0,
-        0,
-        NULL,
-        0
-    FROM inserted;
+    SET NOCOUNT ON;
+
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN EMPLEADOS e ON i.IdEmpleado = e.IdEmpleado
+        INNER JOIN NIVELES n ON e.IdNivel = n.IdNivel
+        WHERE n.IdTipoEnsayo IS NOT NULL
+          AND n.IdTipoEnsayo <> i.IdTipoEnsayo
+    )
+    BEGIN
+        THROW 50001, 'El empleado no esta habilitado para realizar este tipo de ensayo.', 1;
+    END
+END;
+GO
+
+DROP TRIGGER IF EXISTS TR_Normalizar_Observaciones_Registros;
+GO
+
+CREATE TRIGGER TR_Normalizar_Observaciones_Registros
+ON REGISTROS
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE r
+    SET Observaciones = 'SIN OBSERVACIONES'
+    FROM REGISTROS r
+    INNER JOIN inserted i ON r.IdRegistro = i.IdRegistro
+    WHERE i.Observaciones IS NULL
+       OR LTRIM(RTRIM(i.Observaciones)) = '';
+END;
+GO
+
+DROP TRIGGER IF EXISTS TR_Normalizar_Clase_Herramientas;
+GO
+
+CREATE TRIGGER TR_Normalizar_Clase_Herramientas
+ON HERRAMIENTAS
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE h
+    SET Clase = 'NO APLICA'
+    FROM HERRAMIENTAS h
+    INNER JOIN inserted i ON h.IdHerramienta = i.IdHerramienta
+    WHERE LTRIM(RTRIM(i.Clase)) = '';
 END;
 GO
